c41af4970a8ed6a2feb9401782ff8f4d7753c4d2
astowner <the1samevl@gmail.com>
2024-08-14 14:24:54 +0300

    Fixed create user when authorized from google

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -10,48 +12,49 @@ class LoginForm(forms.Form):
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(
-        label="Password",
+        label='Password',
         widget=forms.PasswordInput
     )
     password2 = forms.CharField(
-        label="Repeate password",
+        label='Repeat password',
         widget=forms.PasswordInput
     )
 
     class Meta:
         model = get_user_model()
-        fields = ["username", "first_name", "email"]
+        fields = ['username', 'first_name', 'email']
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
-            raise forms.ValidationError("Password don't match.")
-        return cd["password2"]
-    
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
+
     def clean_email(self):
-        data = self.cleaned_data["email"]
+        data = self.cleaned_data['email']
         if User.objects.filter(email=data).exists():
-            raise forms.ValidationError("Email already in use.")
+            raise forms.ValidationError('Email already in use.')
         return data
-    
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ["first_name", "last_name", "email"]
+        fields = ['first_name', 'last_name', 'email']
 
     def clean_email(self):
-        data = self.cleaned_data["email"]
+        data = self.cleaned_data['email']
         qs = User.objects.exclude(
             id=self.instance.id
         ).filter(
             email=data
         )
         if qs.exists():
-            raise forms.ValidationError("Email already in use.")
+            raise forms.ValidationError('Email already in use.')
         return data
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["date_of_birth", "photo"]
+        fields = ['date_of_birth', 'photo']
--- account/views.py
+++ account/views.py
@@ -6,58 +6,53 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
 
+
+
 def user_login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(
                 request,
-                username=cd["username"],
-                password=cd["password"]
+                username=cd['username'],
+                password=cd['password'],
             )
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse("Authenticated successfully")
+                    return HttpResponse('Authenticated successfully')
                 else:
-                    return HttpResponse("Disabled account")
+                    return HttpResponse('Disabled account')
             else:
-                return HttpResponse("Invalid login")
+                return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(
-        request,
-        "account/login.html",
-        {"form": form}
-    )
-
+    return render(request, 'account/login.html', {'form': form})
 
 def register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create new user, not saving yet
+            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set user password
-            new_user.set_password(
-                user_form.cleaned_data["password"]
-            )
-            # Save the user object
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
             new_user.save()
-            # Create user
+            # Create the user profile
             Profile.objects.create(user=new_user)
             return render(
                 request,
-                "account/register_done.html",
-                {"new_user": new_user}
+                'account/register_done.html',
+                {'new_user': new_user},
             )
     else:
         user_form = UserRegistrationForm()
     return render(
         request,
-        "account/register.html",
-        {"user_form": user_form}
+        'account/register.html',
+        {'user_form': user_form}
     )
 
 @login_required
@@ -70,7 +65,7 @@ def dashboard(request):
 
 @login_required
 def edit(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_form = UserEditForm(
             instance=request.user,
             data=request.POST
@@ -78,28 +73,22 @@ def edit(request):
         profile_form = ProfileEditForm(
             instance=request.user.profile,
             data=request.POST,
-            files=request.FILES
+            files=request.FILES,
         )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(
                 request,
-                "Profile updated successfully"
+                'Profile updated successfully'
             )
         else:
-            messages.error(
-                request,
-                "Error updating your profile"
-            )
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(
         request,
-        "account/edit.html",
-        {
-            "user_form": user_form,
-            "profile_form": profile_form
-        }
+        'account/edit.html',
+        {'user_form': user_form, 'profile_form': profile_form},
     )
